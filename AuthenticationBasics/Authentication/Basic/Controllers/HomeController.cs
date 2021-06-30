c612a13c5ae4ea04360deab81153438041296c1b
Farzin Faghirnavaz <FFaghirnavaz@equisoft.com>
2021-06-30 14:26:15 -0400

    Episode one finished

---
+++ AuthenticationBasics/Authentication/Basic/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Authenticate()
+        {
+            var webTechClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Farzin"),
+                new Claim(ClaimTypes.Email,"Farzin@gmail.com")
+            };
+            var webtechIdentity = new ClaimsIdentity(webTechClaims, "Webtech Identity");
+
+            var licenceClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Farzin Faghir"),
+                new Claim("DrivingLicenes","A+")
+            };
+            var licenceIdentity = new ClaimsIdentity(licenceClaims, "Licence Identity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { webtechIdentity, licenceIdentity });
+
+            await HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
