e80a0bcfced71728c9440e89bef355babc4e959c
Farzin Faghirnavaz <FFaghirnavaz@equisoft.com>
2021-07-04 22:54:57 -0400

    Oauth Episode 01

---
+++ AuthenticationBasics/Authentication/Server/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Server
+{
+    public static class Constants
+    {
+        public static string Audiance = "https://localhost:44312/";
+        public static string Issuer = Audiance;
+        public static string Secret = "not_too_short_secret_otherwise_it_111";
+
+    }
+}
---
+++ AuthenticationBasics/Authentication/Server/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,"Some_i"),
+                new Claim("WebTechId","id")
+            };
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+            var signingCredentials = new SigningCredentials(key, algorithm);
+            var token = new JwtSecurityToken(Constants.Audiance,
+                                           Constants.Issuer,
+                                           claims,
+                                           DateTime.Now,
+                                           DateTime.Now.AddDays(1),
+                                           signingCredentials);
+
+            var tokenJson = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new { access_token = tokenJson });
+        }
+    }
+}
