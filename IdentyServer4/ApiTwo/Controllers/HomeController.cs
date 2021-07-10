f70e80d513a9d366363d94b2d46febca9762c4cc
Farzin Faghirnavaz <FFaghirnavaz@equisoft.com>
2021-07-10 11:29:38 -0400

    IdentityServer 01

---
+++ IdentyServer4/ApiTwo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+
+namespace ApiTwo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("/")]
+        public async Task<IActionResult> Index()
+        {
+            //retreve access token
+            
+            var serverClient = _httpClientFactory.CreateClient();
+
+            var discoveryDocument = await serverClient.GetDiscoveryDocumentAsync("https://localhost:44378/");
+
+            var tokenResponse = await serverClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = "client_id",
+                ClientSecret = "clinet_secret_123",
+                Scope = "ApiOne",
+            });
+
+            //retreve secret data 
+            var apiClient = _httpClientFactory.CreateClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:44328/secret");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return Ok(new
+            {
+                access_token = tokenResponse.AccessToken,
+                message = content
+            });
+        }
+    }
+}
---
+++ IdentyServer4/IdentityServerSample/ApiOne/Controllers/SecretController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiOne.Controllers
+{
+    //https://nahidfa.com/posts/migrating-identityserver4-to-v4/
+    public class SecretController : Controller
+    {
+        [Route("/secret")]
+        [Authorize]
+        [HttpGet]
+        public string Index()
+        {
+            return "Secret message from ApiOne";
+        }
+    }
+}
