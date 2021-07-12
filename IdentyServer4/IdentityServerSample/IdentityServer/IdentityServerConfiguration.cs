fd5935df17a167ab7b11d6dcb79cde3770aebf0b
Farzin Faghirnavaz <FFaghirnavaz@equisoft.com>
2021-07-11 22:34:31 -0400

    IdentityServer_02

--- IdentyServer4/IdentityServerSample/IdentityServer/IdentityServerConfiguration.cs
+++ IdentyServer4/IdentityServerSample/IdentityServer/IdentityServerConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -8,6 +9,12 @@ namespace IdentityServer
     //https://nahidfa.com/posts/migrating-identityserver4-to-v4/
     public static class IdentityServerConfiguration
     {
+        public static IEnumerable<IdentityResource> GetIdentityResources () =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
         public static IEnumerable<ApiScope> GetApiScopes()
         {
             return new List<ApiScope>
@@ -21,6 +28,10 @@ namespace IdentityServer
                 new ApiResource("ApiOne")
                 {
                    Scopes = new []{ "ApiOne" }
+                },
+                new ApiResource("ApiTwo")
+                {
+                   Scopes = new []{ "ApiTwo" }
                 }
             };
 
@@ -34,6 +45,18 @@ namespace IdentityServer
                     AllowedGrantTypes=GrantTypes.ClientCredentials,
                     AllowedScopes={"ApiOne"},
 
+                },
+                new Client
+                {
+                    ClientId="client_id_mvc",
+                    ClientSecrets={new Secret("clinet_secret_mvc_123".ToSha256())},
+                    AllowedGrantTypes=GrantTypes.Code,
+                    RedirectUris={"https://localhost:44352/signin-oidc" },
+                    AllowedScopes={"ApiOne",
+                                   "ApiTwo",
+                                   IdentityServerConstants.StandardScopes.OpenId,
+                                    IdentityServerConstants.StandardScopes.Profile
+                                  },
                 }
             };
     }
